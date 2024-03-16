3b99e410dda2b351b03b3eec1e5ccc2f7de809b4
nisthajnn <nisthajnn1234@gmail.com>
2024-03-16 11:03:36 +0530

    first commit

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="flex justify-center items-center">
+     
+      <span style={{fontWeight: 700, display: "flex", alignItems: "center", justifyContent: "center" }}> <img src="/heart.png" alt="no-img"width="40px" height="40px"/>
+        Developed By Nistha
+      </span>
+    </div>
+  );
+};
+
+export default Footer;
